2719764544e129db7ec4f8edcaadadf19c630248
insyirahsyamila <insyirahsyamila3800@gmail.com>
2024-12-30 11:21:25 +0800

    Initial commit

---
+++ app/src/main/java/com/example/rainforestcoffee/Login/LoginRegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.rainforestcoffee.Login
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.example.rainforestcoffee.R
+
+class LoginRegisterActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_register)
+    }
+}
