5d8db7622a28190ab325a30e9c172ef409146014
insyirahsyamila <insyirahsyamila3800@gmail.com>
2025-01-16 02:13:42 +0800

    -fix viewmodel

--- app/src/main/java/com/example/rainforestcoffee/Login/View/RegisterFragment.kt
+++ app/src/main/java/com/example/rainforestcoffee/Login/View/RegisterFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.example.rainforestcoffee.Data.User
 import com.example.rainforestcoffee.Login.ViewModel.RegisterViewModel
@@ -21,7 +22,7 @@ private val TAG = "RegisterFragment"
 class RegisterFragment : Fragment() {
 
     private lateinit var binding: FragmentRegisterBinding
-    private lateinit var viewModel: RegisterViewModel
+    private val viewModel by viewModels<RegisterViewModel>()
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
